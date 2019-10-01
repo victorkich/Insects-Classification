82f8bfd55dd876b38833139495e20a6a0b4aaee6
Victor Augusto Kich <victorkich@yahoo.com.br>
2019-10-01 07:47:20 -0300

    Remake

--- Classification.py
+++ Classification.py
@@ -1,32 +1,43 @@
 import pandas as pd
-import numpy as np
 from matplotlib import pyplot as plt
-import sklearn as sk
-from sklearn.neural_network import MLPClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 
+# Values to predict
+P = [[3.2, 4.2], [7.2, 4.1]]
+
+# Select the data base
 df = pd.read_csv('Insetos00.csv', encoding='iso-8859-1', header=None)
 df.drop(0, inplace=True)
-df.sort_values(1, ascending = True,inplace=True)
-df = df.replace('Grasshopper', 1).copy()
-df = df.replace('Katydid', 1).copy()
 
-x = pd.DataFrame(df[1], dtype=float)
-y = pd.DataFrame(df[2], dtype=float)
+x_train = df.iloc[:7,1:3]
+y_train = df.iloc[:7,3]
 
-x_train = df.iloc[:6,0:3]
-y_train = df.iloc[:6,3]
+x_test = df.iloc[7:,1:3]
+y_test = df.iloc[7:,3]
 
-x_test = df.iloc[6:,0:3]
-y_test = df.iloc[6:,3]
+print(df)
 
-classifier = MLPClassifier(activation='relu', solver='sgd', alpha=1e-5, hidden_layer_sizes=(5,2), random_state=1, verbose=1)
-classifier.fit(x_train, y_train)
-print(classifier.score(x_test, y_test), 4)
+# Classification using DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(x_train, y_train)
 
-colors = (0,0,0)
-plt.scatter(x, y, alpha=0.5, c = colors)
-plt.title('Gafanhotos')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.plot(classifier)
-plt.show(block=True)
+# Classification using RandomForestClassifier
+rfor = RandomForestClassifier()
+rfor.fit(x_train, y_train)
+
+# Classification using KNeighborsClassifier
+knn = KNeighborsClassifier()
+knn.fit(x_train, y_train)
+
+# Show the models and yours respective acurracy 
+print ("\nUsing DecisionTree classifier prediction is " + str(clf.predict(P)))
+print("Accuracy: " + str(clf.score(x_test, y_test)) + "\n")
+print ("Using MLPC classifier prediction is " + str(rfor.predict(P)))
+print("Accuracy: "+ str(rfor.score(x_test, y_test)) + "\n")
+print ("Using MLPC classifier prediction is " + str(knn.predict(P)))
+print("Accuracy: " + str(knn.score(x_test, y_test)) + "\n")
+
+#y_train = y_train.replace('Grasshopper', 1).copy()
+#y_train = y_train.replace('Katydid', 2).copy()
